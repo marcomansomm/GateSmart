4a313f2ede27c616ba42e99aa36e16c939112759
Marcoantonio Manso <marco.santos@tascomtecnologia.com.br>
2023-11-21 10:37:57 -0300

    feat: implmentando o bip

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
+#include <time.h>
 
 // Provide the RTDB payload printing info and other helper functions.
 #include <addons/RTDBHelper.h>
 
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID "marcoantonio"
-#define WIFI_PASSWORD "12345678"
+#define WIFI_SSID "uaifai-apolo"
+#define WIFI_PASSWORD "bemvindoaocesar"
 
 /* 2. If work with RTDB, define the RTDB URL and database secret */
 #define DATABASE_URL "https://se-cesar-a3244-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
@@ -44,27 +45,31 @@ bool buttonPressed = false;
 long duration;
 float distanceCm;
 float distanceInch;
+long timezone = -3;
+byte daysavetime = 1;
 
 const int trigPin = 19;
 const int echoPin = 18;
 
-
-#define RED_LED  2
+#define RED_LED 2
 #define YELLOW_LED 4
 #define GREEN_LED 5
+#define BIP_PIN 14
 
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
 
 void piscarLed(int led, int tempoDelay);
 void inicioPortao();
-
+String horatioAtual();
+void bip();
 
 void setup()
 {
     pinMode(RED_LED, OUTPUT);
     pinMode(YELLOW_LED, OUTPUT);
     pinMode(GREEN_LED, OUTPUT);
+    pinMode(BIP_PIN, OUTPUT);
 
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
     pinMode(echoPin, INPUT);
@@ -84,6 +89,9 @@ void setup()
     Serial.println();
 
     Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    Serial.println("Contacting Time Server");
+    configTime(3600 * timezone, daysavetime * 3600, "time.nist.gov", "0.pool.ntp.org", "1.pool.ntp.org");
 
     /* Assign the certificate file (optional) */
     // config.cert.file = "/cert.cer";
@@ -110,21 +118,26 @@ void setup()
 void loop()
 {
     inicioPortao();
+    horatioAtual();
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     // Sets the trigPin on HIGH state for 10 micro seconds
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-  
+
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
 
     // Calculate the distance
-    distanceCm = duration * SOUND_SPEED/2;
+    distanceCm = duration * SOUND_SPEED / 2;
 
-    if(distanceCm < 5.00 && distanceCm != 0){
-        
+    if (distanceCm < 5.00 && distanceCm != 0)
+    {
+        Serial.printf("Set opening... %s\n", Firebase.setBool(fbdo, "/Gate/opening/", true) ? "ok" : fbdo.errorReason().c_str());
+        Serial.printf("Set lastTimeOpened... %s\n", Firebase.setString(fbdo, "/Gate/lastTimeOpened/", horatioAtual()) ? "ok" : fbdo.errorReason().c_str());
+
+        bip();
         piscarLed(RED_LED, 0);
         Serial.println("Abrindo o portão");
         piscarLed(YELLOW_LED, 2500);
@@ -132,47 +145,18 @@ void loop()
         piscarLed(GREEN_LED, 5000);
         Serial.println("Fechando o portão");
         piscarLed(YELLOW_LED, 2500);
-        
-        Serial.printf("Set int... %s\n", Firebase.setInt(fbdo, "/portao/back/", distanceCm) ? "ok" : fbdo.errorReason().c_str());
     }
-    
+    Serial.printf("Set opening... %s\n", Firebase.setBool(fbdo, "/Gate/opening/", false) ? "ok" : fbdo.errorReason().c_str());
+
     // Prints the distance in the Serial Monitor
     Serial.print("Distance (cm): ");
     Serial.println(distanceCm);
 
     delay(1000);
-    /*
-    int currentState = digitalRead(BUTTON_PIN);
-    Serial.println(currentState);
-    delay(1000);
-    
-    if(!digitalRead(BUTTON_PIN))
-    {
-        buttonPressed = true;
-        Serial.printf("Button pressed: %s\n", buttonPressed);
-    }
-    else
-    {
-        buttonPressed = false;
-        Serial.printf("Button pressed: %s\n", buttonPressed);
-    }
-    if(buttonPressed)digitalWrite(RED_LED, HIGH);
-    else digitalWrite(RED_LED, LOW);
-    */
-
-    /*if (millis() - dataMillis > 5000)
-    {
-        estado = !estado;
-
-        digitalWrite(RED_LED, estado);
-        digitalWrite(YELLOW_LED, estado);
-        digitalWrite(GREEN_LED, estado);
-        dataMillis = millis();
-        Serial.printf("Set int... %s\n", Firebase.setInt(fbdo, "/test/int", count++) ? "ok" : fbdo.errorReason().c_str());
-    }*/
 }
 
-void piscarLed(int led, int tempoDelay){
+void piscarLed(int led, int tempoDelay)
+{
     bool estado = false;
 
     digitalWrite(led, !estado);
@@ -180,9 +164,36 @@ void piscarLed(int led, int tempoDelay){
     digitalWrite(led, estado);
 }
 
-void inicioPortao(){
+void inicioPortao()
+{
     digitalWrite(RED_LED, HIGH);
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(GREEN_LED, LOW);
     Serial.println("Portão Fechado");
+}
+
+String horatioAtual()
+{
+    struct tm tmstruct;
+
+    tmstruct.tm_year = 0;
+    getLocalTime(&tmstruct);
+
+    // Criamos uma string formatada
+    String time = String((tmstruct.tm_year) + 1900) + "-" + String((tmstruct.tm_mon) + 1) + "-" + String(tmstruct.tm_mday) + " " + String(tmstruct.tm_hour) + ":" + String(tmstruct.tm_min) + ":" + String(tmstruct.tm_sec);
+
+    return time;
+}
+
+void bip()
+{
+
+    digitalWrite(BIP_PIN, HIGH);
+    delay(500);
+    digitalWrite(BIP_PIN, LOW);
+    delay(500);
+    digitalWrite(BIP_PIN, HIGH);
+    delay(500);
+    digitalWrite(BIP_PIN, LOW);
+    delay(500);
 }
